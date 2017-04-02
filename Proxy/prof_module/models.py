aa997269229a93ddd52a687fd0f0d643a098e41d
Sai Pavan D <saipvn4@gmail.com>
2017-04-02 09:50:18 +0530

    Gen

--- Proxy/prof_module/models.py
+++ Proxy/prof_module/models.py
@@ -14,7 +14,7 @@ class Course(models.Model):
 class RollNumberToken(models.Model):
     roll_number = models.CharField(max_length=10)
     face_token = models.CharField(max_length=50)
-    
+
 class Attendance(models.Model):
     IS_PRESENT = (
 		('0', 'ABSENT'),
@@ -24,7 +24,7 @@ class Attendance(models.Model):
     student = models.ForeignKey(User, on_delete=models.CASCADE, related_name="%(app_label)s_%(class)s_stud")
     date = models.DateField(null=True)	# Date is a string of the form YYYY-MM-DD
     is_present = models.CharField(max_length=1, choices = IS_PRESENT, default='0')
-    
+
 class Query(models.Model):
     STATUS = (
 		('0', 'IN PROGRESS'),
@@ -36,8 +36,7 @@ class Query(models.Model):
     date = models.DateField(null=True)
     query = models.CharField(max_length=500)
     status = models.CharField(max_length=1, choices = STATUS, default='0')
-    
-#=======
+
 
 # class TrainingData(models.Model):
 #     stud = models.ForeignKey(User, on_delete=models.CASCADE, related_name="%(app_label)s_%(class)s_stud")
@@ -49,4 +48,3 @@ class Query(models.Model):
 # class AttendancePhotos(models.Model):
 #     prof = models.ForeignKey(User, on_delete=models.CASCADE, related_name="%(app_label)s_%(class)s_prof")
 #     image = models.FileField(upload_to='attendancephotos/'+str(user_directory_path)+'/%Y/%m/%d/')
-#>>>>>>> dd8a38de297eb7d057e39efa043494e029cdbeda
