5915079b0fc22285adbec14b8fcc3289df1f3a5e
srinidhiprabhu96 <srinidhiprabhu96@gmail.com>
2017-04-10 18:39:41 +0530

    Student tests done

--- Proxy/stud_module/tests.py
+++ Proxy/stud_module/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase, Client
 from auth_module.models import *
+from prof_module.models import *
 from django.contrib.auth.models import User
 from django.http.request import HttpRequest
 
@@ -9,12 +10,14 @@ from django.http.request import HttpRequest
 class StudentTestCase(TestCase):
 	def setUp(self):
 		self.client = Client()
-		u = User.objects.create(username="testuser@smail.iitm.ac.in",first_name="Tester",is_staff='0')
-		u.set_password('123')
-		u.save()
-		u = User.objects.create(username="testprof@iitm.ac.in",first_name="TesterProf",is_staff='1')
-		u.set_password('123')
-		u.save()
+		u1 = User.objects.create(username="testuser@smail.iitm.ac.in",first_name="Tester",is_staff='0')
+		u1.set_password('123')
+		u1.save()
+		u2 = User.objects.create(username="testprof@iitm.ac.in",first_name="TesterProf",is_staff='1')
+		u2.set_password('123')
+		u2.save()
+		c = Course.objects.create(course_id="CS1100",course_name="Computational Engineering",taught_by=u2,taken_by=[u1])
+		c.save()
 	
 	# Name each test case starting with "test_". Only asserts in these functions will be tested.
 	def test_hello_function(self):
@@ -62,6 +65,18 @@ class StudentTestCase(TestCase):
 		response = self.client.get('/stud_home/')
 		self.assertRedirects(response,'/login/',status_code=302, target_status_code=200)
 				
+		
+	def test_stud_course(self):
+		# If student is logged in and requests a course he has taken.
+		self.client.login(username="testuser@smail.iitm.ac.in",password="123")
+		response = self.client.get('/stud_course/CS1100/')
+		self.assertEqual(response.status_code,200)
+		self.assertTemplateUsed(response,'stud_course.html')
+		
+		# If student is logged in and requests a course he doesn't take.
+		response = self.client.get('/stud_course/CS1101/')
+		self.assertRedirects(response,'/login/',status_code=302, target_status_code=200)
+		
 	def test_stud_daily_report(self):
 		# Student enters a valid date and makes post request
 		self.client.login(username="testuser@smail.iitm.ac.in",password="123")
@@ -76,3 +91,46 @@ class StudentTestCase(TestCase):
 		# Making an invalid post request, raises http 404
 		response = self.client.post('/stud_daily_report/')
 		self.assertEqual(response.status_code,404)
+		
+	def test_view_queries(self):
+		# If student is logged in and requests view queries page for a  course he has taken.
+		self.client.login(username="testuser@smail.iitm.ac.in",password="123")
+		response = self.client.get('/view_queries/CS1100/')
+		self.assertEqual(response.status_code,200)
+		self.assertTemplateUsed(response,'view_queries.html')
+		
+		# If student is logged in and requests requests view queries page for a course he doesn't take.
+		response = self.client.get('/view_queries/CS1101/')
+		self.assertRedirects(response,'/login/',status_code=302, target_status_code=200)		
+		
+	def test_raise_query(self):
+		# Student enters a valid date and makes post request
+		self.client.login(username="testuser@smail.iitm.ac.in",password="123")
+		response = self.client.get('/raise_query/CS1100/',{'date':'10/4/2017'})
+		self.assertEqual(response.status_code,200)
+		self.assertTemplateUsed(response,'raise_query.html')
+		
+		# Not passing date
+		self.client.login(username="testuser@smail.iitm.ac.in",password="123")
+		response = self.client.get('/raise_query/CS1100/')
+		self.assertEqual(response.status_code,404)
+		
+		# Making an invalid get request, raises http 404
+		response = self.client.post('/raise_query/')
+		self.assertEqual(response.status_code,404)
+		
+	def test_stud_history(self):
+		# Student enters a valid date and makes post request
+		self.client.login(username="testuser@smail.iitm.ac.in",password="123")
+		response = self.client.post('/stud_history/CS1100/',{'date':'10/4/2017'})
+		self.assertEqual(response.status_code,200)
+		self.assertTemplateUsed(response,'stud_history.html')
+		
+		# Not passing date
+		self.client.login(username="testuser@smail.iitm.ac.in",password="123")
+		response = self.client.post('/stud_history/CS1100/')
+		self.assertEqual(response.status_code,404)
+		
+		# Making get request instead of post request
+		response = self.client.get('/stud_history/CS1100/')
+		self.assertRedirects(response,'/login/',status_code=302, target_status_code=200)
--- Proxy/stud_module/views.py
+++ Proxy/stud_module/views.py
@@ -95,6 +95,11 @@ def stud_daily_report(request):
 def view_queries(request, c_id):
 	user = request.user
 	if user.is_authenticated and not user.is_staff:
+		try:										# Check if the student takes the course
+			c = Course.objects.get(course_id=c_id,taken_by=user)	
+		except Exception as e:
+			messages.error(request,"You are not registered for the course!")
+			return redirect('/login/')
 		queries = Query.objects.filter(course_id=c_id, student=user).order_by('-date')	# Gets the student's queries.
 		if len(queries) == 0:
 			#messages.error(request,"No queries to display")
@@ -114,25 +119,37 @@ def query(request, c_id):
 	if request.method == "POST":
 		if request.user.is_authenticated and not request.user.is_staff:
 			# Use a form for handling boundary cases.
-			print request.POST
-			text = request.POST['query']
-			date_str = request.POST['date']
-			print date_str
+			#print request.POST
+			try:
+				text = request.POST['query']
+				date_str = request.POST['date']
+			except:
+				raise Http404("Enter valid data")
+			#print date_str
 			date = datetime.datetime.strptime(date_str,"%d/%m/%Y")
-			print date
+			#print date
 			q = Query(course_id=c_id,student=request.user,date=date,query=text)
 			q.save()
 			messages.info(request,"Request successfully raised")
 			return render(request, 'stud_course.html',{'course_id':c_id})
+		elif user.is_staff:
+			messages.error(request,"You are not a student!")
+			return redirect('/login/')
 		else:
-			pass # Handle error here
-	return render(request, 'stud_course.html',{'course_id':c_id})
+			messages.error(request,"You are not logged in.")
+			return redirect('/login/')
+	else:
+		messages.error(request,"You are not allowed to view this page.")
+		return redirect('/login/')
 
 # Add a date field in raise query also
 def raise_query(request, c_id):
 	user = request.user
 	if request.method == "GET":
-		date = request.GET['date']
+		try:
+			date = request.GET['date']
+		except:
+			raise Http404("Enter a valid date")
 		if request.user.is_authenticated and not request.user.is_staff:
 			try:										# Check if the student takes the course
 				c = Course.objects.get(course_id=c_id,taken_by=user)	
@@ -157,7 +174,10 @@ def stud_history(request, c_id):
 	if request.method == "POST":
 		if request.user.is_authenticated and not request.user.is_staff:
 			# Control reaches here if the user is a student and is authenticated.
-			date = request.POST['date']
+			try:
+				date = request.POST['date']
+			except:
+				raise Http404("Enter a valid date")
 			str_date = date
 			date = datetime.datetime.strptime(str_date,'%d/%m/%Y').date()
 			date_path = datetime.datetime.strftime(date,"%Y/%m/%d")
