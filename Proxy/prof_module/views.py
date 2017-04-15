75b1d86549943484501a6b37f1841fb12da2a983
Sai Pavan D <saipvn4@gmail.com>
2017-04-15 11:21:47 +0530

    Added comments, requirements.txt and edited readme

--- Proxy/prof_module/views.py
+++ Proxy/prof_module/views.py
@@ -27,36 +27,35 @@ import logging
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Implemented by Pavan
+# Called when prof views the homepage
 @csrf_exempt
 def prof_home(request):
-	# add context as third arg to render
-	if request.user.is_authenticated() and request.user.is_staff:
+	# Similar structure is followed throughout all the views. Similar won't be commented
+	if request.user.is_authenticated() and request.user.is_staff:	# display only if the user is logged in and a prof
 		log = logging.getLogger('prof_module')
 		log.info(request.user.first_name + " Professor went to his home page")
-		# print request.user.username
 		qs = Course.objects.filter(taught_by=request.user)
 		return render(request, 'prof_home.html', {'courses':qs})
-	elif not request.user.is_staff:
+	elif not request.user.is_staff:									# display if the user is logged in but not a prof
 		messages.error(request,"You don't have the required permissions!")
 		return redirect('/login/')
-	else:
+	else:															# display if the user is not logged in
 		messages.error(request,"You are not logged in.")
 		return redirect('/login/')
 
 # Implemented by Pavan
+# Called when prof views the course page
 @csrf_exempt
 def prof_course(request, c_id):
-	# add context as third arg to render
 	if request.user.is_authenticated() and request.user.is_staff:
 		log = logging.getLogger('prof_module')
 		log.info(request.user.first_name + " Professor went to " + c_id + " course page")
 		try:
-			c = Course.objects.get(course_id=c_id,taught_by=request.user)
+			c = Course.objects.get(course_id=c_id,taught_by=request.user)	# get the course corresponding to c_id
 			return render(request, 'prof_course.html',{'course_id':c_id})
 		except Exception as e:
-			#raise Http404("You don't teach the course!")
 			cr = Course.objects.filter(course_id=c_id, taught_by=request.user)
-			if not cr.exists():
+			if not cr.exists():												# raise error if the prof doesn't teach c_id
 				raise Http404("You don't teach the course!")
 	elif not request.user.is_staff:
 		messages.error(request,"You don't have the required permissions!")
@@ -65,45 +64,27 @@ def prof_course(request, c_id):
 		messages.error(request,"You are not logged in.")
 		return redirect('/login/')
 
-# @csrf_exempt
-# def add_stud(request, c_id):
-# 	# add context as third arg to render
-# 	if request.user.is_authenticated:
-# 		return render(request, 'add_stud.html')
-# 	else:
-# 		return redirect('/login/')
-
-# def store_stud: doubt - where to create new student
-
 # Implemented by Pavan
+# Called when prof views the day-wise report
 @csrf_exempt
 def daily_report(request, c_id, y, m, d):
-	# add context as third arg to render
 	if request.user.is_authenticated() and request.user.is_staff:
 		cr = Course.objects.filter(course_id=c_id, taught_by=request.user)
-		if not cr.exists():
+		if not cr.exists():		# to ensure that the prof teaches the course 'c_id'. This is present in all course-wise views
 			raise Http404("You don't teach the course!")
-		# if request.method == 'POST':
-		# 	obj = Attendance.objects.get(id = request.POST.get('id'))
-		# 	if obj.is_present == '0':
-		# 		obj.is_present = '1'
-		# 	else:
-		# 		obj.is_present = '0'
-		# 	obj.save()
 		date_str = y+'/'+m+'/'+d
 		log = logging.getLogger('prof_module')
 		log.info(request.user.first_name + " Professor saw the daily report of " + c_id + " corresponding to the date " + date_str)
-		# print os.path.join(BASE_DIR, 'media/'+request.user.username+'/'+c_id+'/'+date_str+'/*')
-		l =  glob(os.path.join(BASE_DIR, 'media/'+request.user.username+'/'+c_id+'/'+date_str+'/*'))
+		l =  glob(os.path.join(BASE_DIR, 'media/'+request.user.username+'/'+c_id+'/'+date_str+'/*'))	# get all the images for the course 'c_id' on the date 'date_str'
 		files = []
 		for i in l:
-			files.append(os.path.basename(i))
+			files.append(os.path.basename(i))		# get the corresponding file names
 		try:
-			date_obj = datetime.date(int(y),int(m),int(d))
+			date_obj = datetime.date(int(y),int(m),int(d))	# create date obj
 		except Exception as e:
 			raise Http404("Invalid date!")
 		try:
-			query_set = Attendance.objects.filter(course_id=c_id,prof=request.user,date=date_obj)
+			query_set = Attendance.objects.filter(course_id=c_id,prof=request.user,date=date_obj)	# get attendance data
 			return render(request, 'daily_report.html',{'course_id':c_id, 'date':date_obj, 'date_url':date_str, 'files':files, 'attendance':query_set})
 		except Exception as e:
 			raise e
@@ -116,34 +97,25 @@ def daily_report(request, c_id, y, m, d):
 		return redirect('/login/')
 
 # Implemented by Pavan
+# Called when prof submits the date in view history.
 @csrf_exempt
 def prof_history(request, c_id):
-	# add context as third arg to render
 	if request.user.is_authenticated() and request.user.is_staff:
 		log = logging.getLogger('prof_module')
 		log.info(request.user.first_name + " Professor saw the history of a " + c_id + " course")
 		cr = Course.objects.filter(course_id=c_id, taught_by=request.user)
 		if not cr.exists():
-			#raise Http404("You dont teach the course!")
 			cr = Course.objects.filter(course_id=c_id, taught_by=request.user)
 			if not cr.exists():
 				raise Http404("You don't teach the course!")
 		try:
-			# query_set = Attendance.objects.filter(course_id=c_id,prof=request.user)
-			# dates = []
-			# for i in query_set:
-			# 	tmp_date = i.date.strftime('%Y/%m/%d')
-			# 	if tmp_date not in dates:
-			# 		dates.append(tmp_date)
-			# dates.sort(reverse=True)
-			if request.method == 'POST':
-				date_str = request.POST.get('date')
-				date_obj = datetime.datetime.strptime(date_str, '%d/%m/%Y')
-				date_str = date_obj.strftime('%Y/%m/%d')
+			if request.method == 'POST':				# submitting date is a post request
+				date_str = request.POST.get('date')		# get the date
+				date_obj = datetime.datetime.strptime(date_str, '%d/%m/%Y')		# parse it
+				date_str = date_obj.strftime('%Y/%m/%d')	# format it as reqd
 				return redirect('/daily_report/'+c_id+'/'+date_str+'/')
 			else:
 				raise Http404("You Haven't entered any date!")
-			# return render(request, 'prof_history.html',{'course_id':c_id})
 		except Exception as e:
 			# raise e
 			raise Http404("You entered the wrong date format!")
@@ -155,6 +127,8 @@ def prof_history(request, c_id):
 		return redirect('/login/')
 
 # Implemented by Pavan
+# Called when prof views take attendance
+# This is just a view request. The uploaded pics will be 'post'ed to take_attendance()
 def upload_class_photos(request, c_id):
 	if request.user.is_authenticated() and request.user.is_staff:
 		log = logging.getLogger('prof_module')
@@ -171,13 +145,9 @@ def upload_class_photos(request, c_id):
 		return redirect('/login/')
 
 # Implemented by Pavan
+# The uploaded images are 'post'ed here
 @csrf_exempt
 def take_attendance(request, c_id):
-	# add context as third arg to render
-	"""cr = Course.objects.filter(course_id=c_id, taught_by=request.user)
-	if not cr.exists():
-		raise Http404("You don't teach the course!")
-		return redirect('/login/')"""
 	if request.user.is_authenticated() and request.user.is_staff and request.method == 'POST':
 		cr = Course.objects.filter(course_id=c_id, taught_by=request.user)
 		if not cr.exists():
@@ -185,16 +155,17 @@ def take_attendance(request, c_id):
 		form = ClassImagesForm(request.POST, request.FILES)
 		if form.is_valid:
 			try:
+				# get date string. parse and format as reqd
 				date_str = request.POST.get('date')
 				date_obj = datetime.datetime.strptime(date_str, '%d/%m/%Y')
 				date_str = date_obj.strftime('%Y-%m-%d')
 				date_str2 = date_obj.strftime('%Y/%m/%d')
 			except Exception as e:
 				raise Http404("You entered the wrong date format!")
-			images = request.FILES.getlist('myfiles')
+			images = request.FILES.getlist('myfiles')	# get the images
 			paths = []
 			for i in images:
-				# print i.name
+				# save them
 				path = default_storage.save(request.user.username+'/'+c_id+'/'+date_str2+'/'+i.name, ContentFile(i.read()))
 				paths += [os.path.join(settings.MEDIA_ROOT, path)]
 
@@ -220,7 +191,6 @@ def take_attendance(request, c_id):
 		messages.error(request,"You don't have the required permissions!")
 		return redirect('/login/')
 	elif request.method != 'POST':
-		#raise Http404("You haven't uploaded any images!")
 		messages.error(request,"You haven't uploaded any images!")
 		return redirect('/login/')
 	else:
@@ -230,7 +200,6 @@ def take_attendance(request, c_id):
 # Implemented by Pavan
 @csrf_exempt
 def prof_queries(request, c_id):
-	# add context as third arg to render
 	if request.user.is_authenticated() and request.user.is_staff:
 		log = logging.getLogger('prof_module')
 		log.info(request.user.first_name + " Professor saw queries for " + c_id)
@@ -239,33 +208,34 @@ def prof_queries(request, c_id):
 			if not cr.exists():
 				raise Http404("You dont teach the course!")
 			if request.method == 'POST':
+				# if the prof wants to change the attendance status
 				res = request.POST.get('query').split()
-				temp_query = Query.objects.get(id= res[0])
-				temp_query.status = '1'
-				temp_query.save()
+				# res[0] is the 'id' and res[1] is the new attendance status.
+				temp_query = Query.objects.get(id= res[0])	# get the query object
+				temp_query.status = '1'		# change the query status
+				temp_query.save()			# save it
 				try:
+					# get the attendance object
 					temp_att = Attendance.objects.get(course_id=c_id,student=temp_query.student,date=temp_query.date)
 				except Exception as e:
 					raise e
-				temp_att.is_present = res[1]
-				temp_att.save()
+				temp_att.is_present = res[1]	# change the attendance status
+				temp_att.save()					# save it
 
-
+			# order the queries in descending order of date.
 			query_set = Query.objects.filter(course_id=c_id).order_by('-date')
-			# print query_set
 			att_set = []
 			for q in query_set:
 				try:
+					# corresponding attendance statuses. They are used in the html.
 					att = Attendance.objects.get(course_id=c_id,student=q.student,date=q.date)
 					att_set.append(att.is_present)
 				except Exception as e:
-					pass
+					raise e
 
-				#print att
 			return render(request, 'prof_queries.html',{'course_id':c_id, 'list':zip(query_set,att_set)})
 		except Exception as e:
-			raise e
-			#raise Http404("You don't teach the course!")
+			# raise e
 			raise Http404("You don't teach the course!")
 	elif not request.user.is_staff:
 		messages.error(request,"You don't have the required permissions!")
@@ -275,22 +245,26 @@ def prof_queries(request, c_id):
 		return redirect('/login/')
 
 # Implemented by Pavan
+# Called when the prof hits 'view_images' in the 'view_queries' page.
+# Supposed to display images corresponding to the query.
 @csrf_exempt
 def view_images(request):
 	if request.user.is_authenticated() and request.user.is_staff:
 		cr = Course.objects.filter(course_id=request.POST.get('course'), taught_by=request.user)
 		if not cr.exists():
 			raise Http404("You don't teach the course!")
-		if request.method == 'POST':
+		if request.method == 'POST':	# data is 'post'ed
+			# get date, course
 			date_str = request.POST.get('date')
-			# user = request.POST.get('username')
 			course = request.POST.get('course')
+			# parse and format the date as reqd
 			date_obj = datetime.datetime.strptime(date_str, '%B %d, %Y')
 			date_str = date_obj.strftime('%Y/%m/%d')
+			# get the file names
 			images = [os.path.basename(x) for x in glob(os.path.join(BASE_DIR, 'media/'+request.user.username+'/'+course+'/'+date_str+'/*'))]
 			for i in range(len(images)):
+				# send the file path via the context
 				images[i] = request.user.username+'/'+course+'/'+date_str+'/' + images[i]
-			# print images
 			return render(request, 'view_images.html', {'list':images,'course':course})
 		else:
 			raise Http404('You haven\'t selected the query!')
--- README.md
+++ README.md
@@ -2,68 +2,75 @@ This project is done as a part of CS3410 in the Jan - May 2017 semester at IIT M
 
 This project is done by Group 9. The group members are -
 
-	Srinidhi Prabhu, CS14B028, srinidhiprabhu96@gmail.com, +919480258046, +919790463346
-	Pavan
-	Vinod
-	Satish
-	Hemanth
+* Srinidhi Prabhu, CS14B028, srinidhiprabhu96@gmail.com, +919480258046, +919790463346
+* Sai Pavan D, CS14B041, saipvn4@gmail.com, +91 9790464704
+* Vinod
+* Satish
+* Hemanth
 
-This project needs django to run. Please look at the requirements.txt file for more details.  
+This project needs django to run. To install, please run :
+	`sudo pip2 install -r requirements.txt`
 
-To run the server,  
-cd Proxy  
-python manage.py runserver  
+To run the server:  
+	`cd Proxy`  
+	`python manage.py runserver`
+
+If running the server for the first time, you may have to run migrations:
+	`python manage.py makemigrations auth_module stud_module prof_module`
+	`python manage.py migrate`
 
 Then on your browser type the address(for example 127.0.0.1:8080) to go to the login page.  
 
 We use the default Django "User" model (in django.contrib.auth.models) to store the users. This makes logging in and logging out, checking if the person trying to access the page is logged in etc. easier. We use the is_staff attribute to check if the user is a professor or a student.
 
 To make sure that changes in models(models are similar to tables for the DB) take place, do:  
-	1) python manage.py check  
-	2) python manage.py makemigrations <app_name>  
-	3) python manage.py migrate  
+	`python manage.py check`  
+	`python manage.py makemigrations <module_name>`
+	`python manage.py migrate`  
 	In case of any errors, resolve them.  
-	
-To access the DB from command line:  
-	1) python manage.py shell - This will open a python command prompt. In that, you can import the models you want and change the DB.  
 
-AUTH MODULE  	
-	Things that can be improved in future in auth_module:
-		1) Delete the entry from SignUp DB in 24 hours( or come up with a hack that stores the time stamp and adds to DB only if the timestamps are >24 hours)   
+To access the DB from command line:  
+	`python manage.py shell`   
+	This will open a python command prompt. In that, you can import the models you want and change the DB.  
+
+AUTH MODULE  	  
+	Things that can be improved in future in auth_module:   
+		1) Delete the entry from SignUp DB in 24 hours( or come up with a hack that stores the time stamp and adds to DB only if the timestamps are > 24 hours)   
 		2) Currently, there doesn't seem to be any way to check if the e-mail ID is valid(For ex: cs14b068@smail.iitm.ac.in is not valid, but our module will send the mail and display "Verification mail sent"). This is a possible source of crashing the DB. Since we only do a regex check, a malicious program could signup many times with different random addresses that match the regex. If possible, this thing should be looked into. We tried out a few tools, but they did not work.  
-	
-	AUTH module features implemented(fully done):
-		1) If email already present in DB, doesn't create a new row, just says mail resent and resends the mail.  
+
+  AUTH module features implemented(fully done)  
+	        1) If email already present in DB, doesn't create a new row, just says mail resent and resends the mail.  
 		2) Reg-Ex matching and giving account type(prof or student)  
 		3) In case email doesnt match regex, or some failure in connection etc. occurs, takes back to signup page and displays the message.  
 		4) Click on link in e-mail, if the code exists, enter the password and signup process will be finished.  
 		5) Password is stored as hash.  
 		6) If the verification link is clicked again after succesful signup, it will say that the URL has already been modified.  
 		7) If a person attempts to signup for an email which already exists as a user, the appropriate message is displayed.  
-		
-ADMIN SIDE
+
+ADMIN SIDE   
 	The functions for the admin are included in the "admin_functions" folder in the "functions.py" file. Further instructions to run this script and other details are provided in the file.
 
 Common static includes all the static files such as JS and CSS files used across the various HTML pages.
 
-dummy_entries.py
+dummy_entries.py  
 	This file includes code to add some database entries. To run the script:
-		1) "python manage.py shell"
-		2) "execfile("dummy_entries.py")
+	`python manage.py shell`
+	In the shell, run:
+	`execfile("dummy_entries.py")`
 	If there are errors, manually copy-paste the lines of the code from the file into the shell to add the database entries.
 
 All photos uploaded by a professor are included in the "media" folder.
 
-sendEmail.py
+sendEmail.py   
 	This file includes code to send an email. This is called as a separate process during the signup process. More details(regarding implementation) can be found in the file.
 
-searchFace.py
+searchFace.py  
 	This file includes code to detect faces in an image and search for the detected faces in a course. This is also called as a separate process when a professor uploads photos to take attendance. More details can be found in the file.
-	
-STUD MODULE
+
+STUD MODULE   
 	Includes functions called when a student is logged in.
-	
-PROF MODULE
+
+PROF MODULE  
 	Includes functions called when a professor is logged in.
-	
+
 Issues have been created on github to keep track of what things were to be done. Feel free to raise more issues, if needed
